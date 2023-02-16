aa267bc0152b79aa0f035509407c56497ef00f7d
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2023-02-15 19:55:44 -0600

    Login Avatar added, Login name remove alt={username}, Avatar CSSd

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -13,18 +13,19 @@ function Header() {
     const navigate = useNavigate()        // ALLOWS US TO ACCESS HISTORY
 
     // ANYTIME YOU SEE useSelector, WE ARE GOING INTO THE Store.JS AND ACCESSING THE DATA 
-    const username = useSelector(selectUserName)    // USES THE useSelector TO HAVE ACCESS TO THE USERNAME 
+    const userName = useSelector(selectUserName)    // USES THE useSelector TO HAVE ACCESS TO THE USERNAME 
     const userPhoto = useSelector(selectUserPhoto)  // USES THE useSelector TO HAVE ACCESS TO THE PHOTO
 
     const handleAuth = () => {
         auth.signInWithPopup(provider).then((result) => {
             console.log(result);
-            setUser(result.user)
+            setUser(result.user) // THIS WILL ALLOW YOU TO SET A NEW USER 
         }).catch((error) => {
             alert(error.message)
         })
     }
 
+    // THE FUNCTION FOR CREATING A NEW USER 
     const setUser = (user) => {
         dispatch(
             setUserLoginDetails({
@@ -39,15 +40,34 @@ function Header() {
     <div className='header'>
         <div className='header-container'>
             <a className='logo' href='#'><img src='/images/logo.svg' alt='' /></a>
-            <div className='navbar'>
+            {
+                !userName ? (
+                    <a className='header-button' onClick={handleAuth}>LOGIN</a>
+                ) : (
+                <>
+                    <div className='navbar'>
+                        <a href='/home' className='navlink'><img  src='/images/home-icon.svg' className='nav-img' /><span>HOME</span></a>
+                        <a href='#' className='navlink'><img src='/images/search-icon.svg' className='nav-img' /><span>SEARCH</span></a>
+                        <a href='#' className='navlink'><img src='/images/watchlist-icon.svg' className='nav-img' /><span>WATCHLIST</span></a>
+                        <a href='#' className='navlink'><img src='/images/original-icon.svg' className='nav-img' /><span>ORIGINALS</span></a>
+                        <a href='#' className='navlink'><img src='/images/movie-icon.svg' className='nav-img' /><span>MOVIES</span></a>
+                        <a href='#' className='navlink'><img src='/images/series-icon.svg' className='nav-img' /><span>SERIES</span></a>
+                    </div>
+
+                    <img className='profile-pic' src={userPhoto}  />
+                    
+                </>
+                )
+            }
+            {/* <div className='navbar'>
                 <a href='/home' className='navlink'><img  src='/images/home-icon.svg' className='nav-img' /><span>HOME</span></a>
                 <a href='#' className='navlink'><img src='/images/search-icon.svg' className='nav-img' /><span>SEARCH</span></a>
                 <a href='#' className='navlink'><img src='/images/watchlist-icon.svg' className='nav-img' /><span>WATCHLIST</span></a>
                 <a href='#' className='navlink'><img src='/images/original-icon.svg' className='nav-img' /><span>ORIGINALS</span></a>
                 <a href='#' className='navlink'><img src='/images/movie-icon.svg' className='nav-img' /><span>MOVIES</span></a>
                 <a href='#' className='navlink'><img src='/images/series-icon.svg' className='nav-img' /><span>SERIES</span></a>
-            </div>
-            <a className='header-button' onClick={handleAuth}>LOGIN</a>
+            </div> */}
+            {/* <a className='header-button' onClick={handleAuth}>LOGIN</a> */}
         </div>
     </div>
   )
